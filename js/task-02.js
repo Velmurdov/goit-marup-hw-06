9d02d483cd1ba91808c618f527e5580324db4f2b
Velmurdov <batuvalll@gmail.com>
2023-06-25 15:01:14 +0200

    updt 2 js

--- js/task-02.js
+++ js/task-02.js
@@ -11,10 +11,14 @@ const ingredients = [
 const listEl = document.querySelector("#ingredients");
 
 const markup = function(array){
-  array.map(elem=>{
+  const markupArray = array.map(elem=>{
     const liEl = document.createElement('li');
     liEl.textContent = elem ;
     liEl.classList.add('item');
-    return listEl.append(liEl);
+    return liEl.outerHTML;
   });
+  
+  const markupString = markupArray.join('');
+  listEl.innerHTML = markupString;
 }(ingredients);
+
